dcf93fd7609e90fd3e7ce20f6484b4f975c73f00
mauricio1499 <dantee-mauri@hotmail.com>
2020-02-01 21:09:57 -0500

    jwt_3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
+from flask_cors import CORS
 
 
 
@@ -18,6 +19,9 @@ users = [
     User(2, 'user2', 'abcxyz'),
     User(3, 'admin', 'admin'),
     User(4, 'root', 'root'),
+    User(5, 'ruiz', 'ruiz'),
+    User(6, 'admin', '1234'),
+    User(7, 'mauricio', '1234'),
 ]
 
 username_table = {u.username: u for u in users}
@@ -32,10 +36,12 @@ def authenticate(username, password):
 def identity(payload):
     user_id = payload['identity']
     return userid_table.get(user_id, None)
+###################---FLASK-----###################################
 
 app = Flask(__name__)
 app.debug = False
 app.config['SECRET_KEY'] = 'super-secret'
+CORS(app)
 
 jwt = JWT(app, authenticate, identity)
 
@@ -46,9 +52,7 @@ def protected():
 
 @app.route('/')
 def index():
-    return '<b>Hello</b>'
-
-
+    return '<h3> <b> Conociendo Python</b></h3><p> ABC Community </p>'
 
 
 if __name__ == '__main__':
